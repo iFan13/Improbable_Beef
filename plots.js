fd971c9dbfae92eb70e3f1a334d615f8ef49befc
Ian Fan <ian.fan13@gmail.com>
2021-03-19 15:47:31 -0400

    12.4.3

--- plots.js
+++ plots.js
@@ -1,71 +1,35 @@
-
-d3.json("samples.json").then(function(data){console.log(data)});
-d3.json("samples.json").then(function(data){
-    //get all wash frequency from metadata
-    wfreq = data.metadata.map(person => person.wfreq)
-    //sort descending
-    .sort(
-            (a,b) => b - a);
-    //filter non zero/aka nulls
-        filteredWfreq = wfreq.filter(element => element != null);
-    console.log(filteredWfreq);
-});
-
-d3.json("samples.json").then(function(data){
-    firstPerson = data.metadata[0];
-    Object.entries(firstPerson).forEach(([key, value]) =>
-      {console.log(key + ': ' + value);});
-});
-
-d3.selectAll("#selectOption").on("change", updatePage);
-
-function updatePage() {
-  var dropdownMenu = d3.selectAll("#selectOption").node();
-  var dropdownMenuID = dropdownMenu.id;
-  var selectedOption = dropdownMenu.value;
-
-  console.log(dropdownMenuID);
-  console.log(selectedOption);
-};
-
-//skill drill
-d3.selectAll("#selectDisney").on("change", disneyCharacter);
-
-function disneyCharacter(){
-    var dropdownDisney = d3.selectAll("#selectDisney").node();
-    console.log(dropdownDisney.value)
-}
-
-
-
 function init() {
-    data = [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16] }];
-    Plotly.newPlot("plot", data);
-  };
+    var selector = d3.select("#selDataset");
   
-  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-  function updatePlotly() {
-    var dropdownMenu = d3.select("#dropdownMenu");
-    var dataset = dropdownMenu.property("value");
-  
-    var xData = [1, 2, 3, 4, 5];
-    var yData = [];
-  
-    if (dataset === 'dataset1') {
-      yData = [1, 2, 4, 8, 16];
-    };
-  
-    if (dataset === 'dataset2') {
-      yData = [1, 10, 100, 1000, 10000];
-    };
-  
-    var trace = {
-      x: [xData],
-      y: [yData],
-    };
-    Plotly.restyle("plot", trace);
-  };
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
   
   init();
+
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
+  
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      for (category in result) {
+          PANEL.append("h6").text(category.toUpperCase()+": "+result[category])
+      }
+    
+    });
+  }
