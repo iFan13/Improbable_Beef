54a8105bdf7ffbc8b72fe14256ebd00c83cc383a
Ian Fan <ian.fan13@gmail.com>
2021-03-19 15:32:00 -0400

    12.4.2

--- plots.js
+++ plots.js
@@ -1,3 +1,4 @@
+
 d3.json("samples.json").then(function(data){console.log(data)});
 d3.json("samples.json").then(function(data){
     //get all wash frequency from metadata
@@ -34,3 +35,37 @@ function disneyCharacter(){
     var dropdownDisney = d3.selectAll("#selectDisney").node();
     console.log(dropdownDisney.value)
 }
+
+
+
+function init() {
+    data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }];
+    Plotly.newPlot("plot", data);
+  };
+  
+  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+  function updatePlotly() {
+    var dropdownMenu = d3.select("#dropdownMenu");
+    var dataset = dropdownMenu.property("value");
+  
+    var xData = [1, 2, 3, 4, 5];
+    var yData = [];
+  
+    if (dataset === 'dataset1') {
+      yData = [1, 2, 4, 8, 16];
+    };
+  
+    if (dataset === 'dataset2') {
+      yData = [1, 10, 100, 1000, 10000];
+    };
+  
+    var trace = {
+      x: [xData],
+      y: [yData],
+    };
+    Plotly.restyle("plot", trace);
+  };
+  
+  init();
