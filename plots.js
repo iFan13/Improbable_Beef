cadc0a31e046a1dcb7912571314f320cc93d4a94
Ian Fan <ian.fan13@gmail.com>
2021-03-19 15:25:59 -0400

    12.4.1

--- plots.js
+++ plots.js
@@ -15,3 +15,22 @@ d3.json("samples.json").then(function(data){
     Object.entries(firstPerson).forEach(([key, value]) =>
       {console.log(key + ': ' + value);});
 });
+
+d3.selectAll("#selectOption").on("change", updatePage);
+
+function updatePage() {
+  var dropdownMenu = d3.selectAll("#selectOption").node();
+  var dropdownMenuID = dropdownMenu.id;
+  var selectedOption = dropdownMenu.value;
+
+  console.log(dropdownMenuID);
+  console.log(selectedOption);
+};
+
+//skill drill
+d3.selectAll("#selectDisney").on("change", disneyCharacter);
+
+function disneyCharacter(){
+    var dropdownDisney = d3.selectAll("#selectDisney").node();
+    console.log(dropdownDisney.value)
+}
